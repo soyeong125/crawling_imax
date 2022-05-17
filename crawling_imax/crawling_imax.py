a5d445fafcecb312a0d497afe8f856402d51569a
SoYeong Choi <thdud3410@gmail.com>
2022-05-17 23:06:05 +0900

    영화 이름 가져오기

--- crawling_imax/crawling_imax.py
+++ crawling_imax/crawling_imax.py
@@ -2,8 +2,12 @@
 import requests
 from bs4 import BeautifulSoup #웹 크롤링에 필요한 모듈 설치
 
-url = 'https://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20220602' #현재 안됨;; 왜 ㅡㅡ
+url = 'https://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20220517' #현재 안됨;; 왜 ㅡㅡ
+#url = 'http://www.cgv.co.kr/theaters/?areacode=01&theaterCode=0056&date=20220517' #현재 안됨;; 왜 ㅡㅡ
 html = requests.get(url)
-print(html.text)
+
+soup = BeautifulSoup(html.text, 'html.parser')
+title_list = soup.select('div.info-movie')
+print(title_list)
 
 
